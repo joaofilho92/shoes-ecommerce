fc59028536e689f282c4b4e36f8d9e36ff1a4d66
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-08-18 10:58:26 +0200

    Nav and products

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,14 +1,30 @@
 import './Nav.css'
+import {FiHeart} from 'react-icons/fi'; 
+import {AiPOutlineShoppingCart, AiOutlineUserAdd, AiOutlineShoppingCart} from 'react-icons/ai';
 
 function Nav() {
   return (
     <nav>
-      <div className='nav-container'>
-        <input type='text' className='search-input' placeholder='Enter your search shoes.'/>
+      <div className="nav-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your search shoes."
+        />
       </div>
-      
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons"/>
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons"/>
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons"/>
+        </a>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Nav;
+export default Nav; 
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,11 +1,26 @@
-import './Products.css';
+import "./Products.css";
+import { AiFillStar } from "react-icons/ai";
 
 function Products() {
   return (
-    <div>
-      <h1>eaee</h1>
-    </div>
-  )
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Card_Image"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar />
+              <span className="total-reviews">4</span>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
 
 export default Products;
