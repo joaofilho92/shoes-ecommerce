17b28264310bab915fca6faf0131de1916da63f4
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-08-18 21:57:17 +0200

    Refactoring, fix and Filtering Apply

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,30 +1,32 @@
-import './Nav.css'
-import {FiHeart} from 'react-icons/fi'; 
-import {AiPOutlineShoppingCart, AiOutlineUserAdd, AiOutlineShoppingCart} from 'react-icons/ai';
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import "./Nav.css";
 
-function Nav() {
+const Nav = ({ handleInputChange, query }) => {
   return (
     <nav>
       <div className="nav-container">
         <input
-          type="text"
           className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
           placeholder="Enter your search shoes."
         />
       </div>
       <div className="profile-container">
         <a href="#">
-          <FiHeart className="nav-icons"/>
+          <FiHeart className="nav-icons" />
         </a>
-        <a href="#">
-          <AiOutlineShoppingCart className="nav-icons"/>
+        <a href="">
+          <AiOutlineShoppingCart className="nav-icons" />
         </a>
-        <a href="#">
-          <AiOutlineUserAdd className="nav-icons"/>
+        <a href="">
+          <AiOutlineUserAdd className="nav-icons" />
         </a>
       </div>
     </nav>
   );
-}
+};
 
-export default Nav; 
+export default Nav;
--- src/SideBar/SideBar.js
+++ src/SideBar/SideBar.js
@@ -3,20 +3,19 @@ import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
 import "./SideBar.css";
 
-function SideBar() {
+const Sidebar = ({ handleChange }) => {
   return (
     <>
       <section className="sidebar">
         <div className="logo-container">
           <h1>🛒</h1>
         </div>
-
-        <Category />
-        <Price />
-        <Colors />
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
       </section>
     </>
   );
-}
+};
 
-export default SideBar;
+export default Sidebar;
